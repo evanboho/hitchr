f8a06ac1a411ab51288252f90ea11b3ee1c32bce
Evan <evan.kuchar@gmail.com>
2012-02-13 13:48:12 -0600

    attempt at updating

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,11 +15,11 @@ Network::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
-
+  
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
