baf078f98dd7f7d12fe9d890e650e2ea6f068eba
Evan Kuchar <evan.kuchar@gmail.com>
2012-02-24 17:57:41 -0600

    commented out the other one

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -10,8 +10,8 @@ ActionMailer::Base.smtp_settings = {
 
 ActionMailer::Base.default_url_options[:host] = "localhost:3000"
 
-# require 'development_mail_interceptor'
+require 'development_mail_interceptor'
 
-config.action_mailer.raise_delivery_errors = true
+# config.action_mailer.raise_delivery_errors = true
 
 Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
