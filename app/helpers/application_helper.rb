fda2c33a883c3296c7b0615fb88a777faabcdd1c
Evan Kuchar <evan.kuchar@gmail.com>
2012-02-25 19:55:29 -0600

    being to implement chosen seletor and being to clean up asset requirements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,4 +28,63 @@ module ApplicationHelper
     # end
   end
   
+  def state_select(form, attribute)
+    state_list = ['AL',
+  					'AK',
+  					'AZ',
+  					'AR',
+  					'CA',
+  					'CO',
+  					'CT',
+  					'DE',
+  					'FL',
+  					'GA',
+  					'HI',
+  					'ID',
+  					'IL',
+  					'IN',
+  					'IA',
+  					'KS',
+  					'KY',
+  					'LA',
+  					'ME',
+  					'MD',
+  					'MA',
+  					'MI',
+  					'MN',
+  					'MS',
+  					'MO',
+  					'MT',
+  					'NE',
+  					'NV',
+  					'NH',
+  					'NJ',
+  					'NM',
+  					'NY',
+  					'NC',
+  					'ND',
+  					'OH',
+  					'OK',
+  					'OR',
+  					'PA',
+  					'RI',
+  					'SC',
+  					'SD',
+  					'TN',
+  					'TX',
+  					'UT',
+  					'VT',
+  					'VA',
+  					'WA',
+  					'WV',
+  					'WI',
+  					'WY']
+  	
+  	form.select(attribute, state_list, {}, {class: "chzn-select"})
+  	#TODO: 1) give a class (e.g. chzn-select to the selectors)
+  	#      2) download chosen plugin 
+  	#      3) get the assets in teh right place
+  	#      4)   
+  end
+  
 end
--- db/migrate/20120209162110_add_nickname_uniqueness_index.rb
+++
@@ -1,9 +0,0 @@
-class AddNicknameUniquenessIndex < ActiveRecord::Migration
-  def up
-    add_index :users, :nickname, :unique => true
-  end
-
-  def down
-    remove_index :users, :nickname
-  end
-end
